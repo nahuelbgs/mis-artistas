2a0020e7f7407f3d1e3275e0851c9d4f994f69af
Nahuel <nahuelbgs@hotmail.com>
2023-09-04 18:13:52 -0300

    Login responsive

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,24 +35,24 @@ function Login() {
     <>
       {accessToken === null ? (
         <div className="flex h-screen">
-          <div className="m-auto w-3/4 h-3/4 flex justify-between bg-[#111928af] backdrop-blur-lg backdrop-saturate-200 border border-[#ffffff20] rounded-lg">
-            <div className="w-5/12 flex flex-col items-start justify-center ml-10 text-white gap-5">
-              <h1 className="text-2xl">
+          <div className="m-auto w-3/4 h-3/4 flex justify-between bg-[#111928af] backdrop-blur-lg backdrop-saturate-200 border border-[#ffffff20] rounded-lg max-lg:flex-col max-lg:justify-around max-sm:w-11/12 max-sm:h-5/6">
+            <div className="w-5/12 flex flex-col items-start justify-center ml-10 text-white gap-5 max-lg:w-full max-lg:h-1/2 max-lg:m-0 max-lg:p-4 max-sm:gap-3">
+              <h1 className="text-2xl max-sm:text-xl">
                 ¿Te gustaría conocer cuáles son los artistas que más has escuchado?
               </h1>
-              <h2 className="text-sm">Muestra tus principales artistas y canciones en Spotify durante el último mes, los últimos seis meses y a lo largo de tu historial de escucha.</h2>
+              <h2 className="text-sm max-sm:text-base">Muestra tus principales artistas y canciones en Spotify durante el último mes, los últimos seis meses y a lo largo de tu historial de escucha.</h2>
               <Button color="success">
                 <a
-                  className="text-xl text-white"
+                  className="text-xl text-white max-sm:text-lg"
                   href={`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://mis-artistas.vercel.app/&scope=user-top-read&grant_type=authorization_code`}
                 >
                   INICIAR SESIÓN CON SPOTIFY
                 </a>
               </Button>
             </div>
-            <div className="w-1/2 mt-3">
+            <div className="w-1/2 mt-3 max-lg:w-full max-lg:h-1/2">
               <img
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover max-lg:object-contain max-lg:p-4"
                 src="https://i.postimg.cc/HxrbP9tN/asdasd.png"
                 alt="Imagen con varios artistas"
               />
